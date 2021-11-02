0088f282daa3c5ab27ff57a103a6684ba38e7252
Kristen Friday <kfriday@nd.edu>
2021-11-02 01:15:37 -0400

    Main file

---
+++ ChatService.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+# ChatService.py
+# Authors: Kristen Friday, Carlo Preciado
+# Date: November 1, 2021
+#
+# ChatService.py serves as the main runner file to initiate
+# the chat service
+
+
+def main():
+    '''Main runner function for service'''
+
+    # TODO: Initialize SuperNode
+
+    # TODO: Initialize LoginServer
+
+    # TODO: Add User nodes to system
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
